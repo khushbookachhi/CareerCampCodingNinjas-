38ff0540211194d3664bcb76ca3577295f16f750
khushbookachhi <123919771+khushbookachhi@users.noreply.github.com>
2023-03-03 14:37:16 +0530

    Update Parallelogram Pattern.java

--- Java Introduction/Lec: 6 Patterns2/Parallelogram Pattern.java
+++ Java Introduction/Lec: 6 Patterns2/Parallelogram Pattern.java
@@ -52,3 +52,13 @@ public class Main {
 
 	}
 }
+// Another Solution*************
+/*for(int i=1; i<=n; i++){
+    for(int j=1; j<=i-1; j++){
+        System.out.print(" ");
+    }
+    for(int j=1; j<=n; j++){
+        System.out.print("*");
+    }
+    System.out.println();
+} */
