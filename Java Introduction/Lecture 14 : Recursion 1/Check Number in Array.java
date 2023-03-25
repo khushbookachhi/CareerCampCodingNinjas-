4e0b17ad032f2aa0c878c8e72634e2fa2cafbc51
khushbookachhi <123919771+khushbookachhi@users.noreply.github.com>
2023-03-25 15:30:53 +0530

    Create Check Number in Array.java

---
+++ Java Introduction/Lecture 14 : Recursion 1/Check Number in Array.java
@@ -0,0 +1,42 @@
+Given an array of length N and an integer x, you need to find if x is present in the array or not. Return true or false.
+Do this recursively.
+Input Format :
+Line 1 : An Integer N i.e. size of array
+Line 2 : N integers which are elements of the array, separated by spaces
+Line 3 : Integer x
+Output Format :
+'true' or 'false'
+Constraints :
+1 <= N <= 10^3
+Sample Input 1 :
+3
+9 8 10
+8
+Sample Output 1 :
+true
+Sample Input 2 :
+3
+9 8 10
+2
+Sample Output 2 :
+false
+  
+  public class Solution {
+	
+	public static boolean checkNumber(int a[], int x) {
+		if(a[0]==x){
+        return true;
+		}
+		if(a.length==1){
+			return(a[0]==x);
+		}
+
+	int smallArray[]= new int[a.length-1];
+	for(int i=1; i<a.length; i++){
+		smallArray[i-1]=a[i];
+	}
+
+		boolean check= checkNumber(smallArray, x);
+	return check;
+	}
+}
