d3563a49977b77334512f95ca5711b0dda197da5
khushbookachhi <123919771+khushbookachhi@users.noreply.github.com>
2023-04-06 01:01:31 +0530

    Update Interesting Alphabets.java

--- Java Introduction/Lec: 5 Patterns/Interesting Alphabets.java
+++ Java Introduction/Lec: 5 Patterns/Interesting Alphabets.java
@@ -50,3 +50,9 @@ public class Solution {
 	
 	}
 }
+// for(int i=n-1; i>=0; i--){
+// 	    for(int j=i; j<n; j++){
+// 	     System.out.print((char)('A'+j));   
+// 	    }
+// 	    System.out.println();
+// 	}
