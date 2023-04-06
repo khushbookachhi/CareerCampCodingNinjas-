ee7c6fa7cfbb41232885682bccfe8c66e678b6fe
khushbookachhi <123919771+khushbookachhi@users.noreply.github.com>
2023-04-07 00:34:52 +0530

    Update Odd Square.java

--- Java Introduction/Lec: 6 Patterns2/Odd Square.java
+++ Java Introduction/Lec: 6 Patterns2/Odd Square.java
@@ -45,3 +45,14 @@ public class Main {
 }
 	}
 }  
+
+************another way////////////
+	for(int i=1; i<=n; i++){
+    for(int j=i; j<=n; j++){
+      System.out.print(2*j-1);  
+    }
+    for(int j=1; j<i; j++){
+        System.out.print(2*j-1);  
+    }
+    System.out.println();
+}
